26a107eca983784b5204fb643ad2d6228aef2679
David Antonio Fajardo Ponce <davidagape@hotmail.com>
2025-11-24 03:23:36 -0600

    Update getdates.js

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,7 +1,18 @@
 function getDates() {
   var date = new Date();
+  let dateOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  };
   let year = date.getFullYear();
-  document.getElementById("currentyear").innerHTML = year;
-  document.getElementById("lastModified").innerHTML = date;
+  document.getElementById("currentyear").innerHTML = `&copy;${year}`;
+  document.getElementById("lastModified").innerHTML = date.toLocaleDateString(
+    "en-US",
+    dateOptions
+  );
 }
 getDates();
+
